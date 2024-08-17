9151a76bddfb9bb87a1427571cfb492b5fde384c
PatricioVargasR <1722110158@utectulancingo.edu.mx>
2024-08-17 09:16:26 -0600

    finally

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 # Importar función para crear el archivo
 from app import crear_aplicacion
 from dotenv import load_dotenv
+import os
 
 # Crear la aplicación en base a la función
 app = crear_aplicacion()
@@ -12,5 +13,5 @@ app = crear_aplicacion()
 load_dotenv()
 
 # Descomentar al utilizar local
- if __name__ == '__main__':
-     app.run(port=os.getenv("PORT", default=5000), host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(port=os.getenv("PORT", default=5000), host='0.0.0.0')
